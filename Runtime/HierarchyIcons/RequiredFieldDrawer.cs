6f26cc08a7588f5048ef04202a656e23f0fb671d
Renier Gaytan <94753366+SrOvni@users.noreply.github.com>
2025-07-07 14:08:22 -0600

    dummy

--- Runtime/HierarchyIcons/RequiredFieldDrawer.cs
+++ Runtime/HierarchyIcons/RequiredFieldDrawer.cs
@@ -1,15 +1,9 @@
-using UnityEngine;
-using UnityEditor;
-
 namespace UtilitiesLibrary
 {
     using UnityEngine;
     using UnityEditor;
     using System;
     using UnityEngine.UI;
-    using System.Runtime.CompilerServices;
-    using PlasticGui.Configuration;
-    using ObservableCollections;
 
     [CustomPropertyDrawer(typeof(RequiredFieldAttribute))]
     public class RequiredFieldDrawer : PropertyDrawer
@@ -20,10 +14,10 @@ namespace UtilitiesLibrary
             Button button = new GameObject("Button").AddComponent<Button>();
             button.onClick.AddListener(() => Debug.Log("Hola mundo"));
 
-            
+
         }
 
-        
+
     }
 
     [AttributeUsage(AttributeTargets.Field)]
